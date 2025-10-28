dacd71d3131a3a77f124c3030d43e0217f6bb0a7
Aditi D <aditid.glb@gmail.com>
2025-10-28 11:43:14 +0530

    Added script for extracting data

---
+++ configs/paths.py
@@ -0,0 +1,9 @@
+import os
+
+# Raw dataset paths (replace with your actual paths)
+RAW_CELEB_DF = r"C:\Users\aditi\Downloads\Celeb-DF-v2(1)"
+RAW_FFPP = r"C:\Users\aditi\OneDrive\Desktop\Aditi D\python\original_sequences"
+
+# Internal project paths
+MANIFEST_FILE = "data/datasets/manifest.csv"
+PROCESSED_DATA_DIR = "data/processed"
